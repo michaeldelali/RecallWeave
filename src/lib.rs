d96a3d2b517f8b68059f2aa87f61ab53124ae47b
michaeldelali <43325182+michaeldelali@users.noreply.github.com>
2020-03-18 11:54:00 +0700

    feat: append-only store

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,4 @@
 //! `recallweave` — a local-first agent memory lifecycle engine.
 //!
 //! This crate is intentionally dependency-free (Rust standard library only). It
+//! models an agent's long-term memory as an append-only, integrity-chained log
