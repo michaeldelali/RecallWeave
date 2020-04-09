bdd5286f41f2b2beb9f677c2d439fb6e89f16767
michaeldelali <43325182+michaeldelali@users.noreply.github.com>
2020-04-09 09:34:00 +0700

    refactor: tidy supersession

--- src/lib.rs
+++ src/lib.rs
@@ -2,3 +2,5 @@
 //!
 //! This crate is intentionally dependency-free (Rust standard library only). It
 //! models an agent's long-term memory as an append-only, integrity-chained log
+//! of typed memories and provides the full lifecycle around them: assertion,
+//! deterministic dedupe, supersession, tombstoning, TTL-based forgetting,
