318f17beeb32770460daebc361a5601ebb7b897e
michaeldelali <43325182+michaeldelali@users.noreply.github.com>
2020-04-09 09:37:00 +0700

    test: cover append-only store

--- src/lib.rs
+++ src/lib.rs
@@ -4,3 +4,4 @@
 //! models an agent's long-term memory as an append-only, integrity-chained log
 //! of typed memories and provides the full lifecycle around them: assertion,
 //! deterministic dedupe, supersession, tombstoning, TTL-based forgetting,
+//! compaction, conflict detection, integrity verification, query/filter, and a
