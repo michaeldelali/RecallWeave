0ce4e84ccada55580cc7db54a6585c1200fbd9b7
michaeldelali <43325182+michaeldelali@users.noreply.github.com>
2020-05-22 11:21:00 +0700

    perf: improve query filters

---
+++ src/main.rs
@@ -0,0 +1,71 @@
+//! The `recallweave` command-line interface.
+//!
+//! A hand-rolled argument parser (no clap; stdlib only) exposes the memory
+//! lifecycle as subcommands. Every command operates on a store directory chosen
+//! with `--dir` (default `.recallweave`). Output is human-readable text by
+//! default, or machine JSON with `--json`, so the CLI is scriptable.
+//!
+//! Run `recallweave help` for the full command list.
+
+use recallweave::json::Json;
+use recallweave::model::{MemoryKind, Provenance};
+use recallweave::query::{Query, Sort};
+use recallweave::store::{now_epoch, AssertSpec, Store};
+use std::collections::VecDeque;
+use std::path::PathBuf;
+use std::process::ExitCode;
+
+fn main() -> ExitCode {
+    let args: Vec<String> = std::env::args().skip(1).collect();
+    match run(args) {
+        Ok(()) => ExitCode::SUCCESS,
+        Err(e) => {
+            eprintln!("error: {}", e);
+            ExitCode::FAILURE
+        }
+    }
+}
+
+fn run(args: Vec<String>) -> Result<(), String> {
+    let mut args: VecDeque<String> = args.into();
+    if args.is_empty() {
+        print_help();
+        return Ok(());
+    }
+    // Global flags may appear before the subcommand (e.g. `--dir X list`).
+    // We scan forward for the first token that is not a flag or a flag value,
+    // treating that as the command and leaving all other tokens for the
+    // per-command flag parser. `--json`, `--all` are known value-less flags.
+    const VALUELESS: &[&str] = &["json", "all", "help", "version"];
+    let mut leading: Vec<String> = Vec::new();
+    let mut cmd: Option<String> = None;
+    while let Some(tok) = args.pop_front() {
+        if let Some(name) = tok.strip_prefix("--") {
+            // A leading --help/--version acts immediately, regardless of order.
+            if name == "help" {
+                print_help();
+                return Ok(());
+            }
+            if name == "version" {
+                println!("recallweave {}", recallweave::VERSION);
+                return Ok(());
+            }
+            leading.push(tok.clone());
+            if !VALUELESS.contains(&name) {
+                // consume this flag's value too
+                if let Some(v) = args.pop_front() {
+                    leading.push(v);
+                }
+            }
+        } else if tok == "-h" {
+            print_help();
+            return Ok(());
+        } else if tok == "-V" {
+            println!("recallweave {}", recallweave::VERSION);
+            return Ok(());
+        } else {
+            cmd = Some(tok);
+            break;
+        }
+    }
+    let cmd = match cmd {
