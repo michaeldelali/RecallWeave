5cc81018b7b386709c87e4f0dba427326b89ead4
michaeldelali <43325182+michaeldelali@users.noreply.github.com>
2021-02-10 16:18:00 +0700

    feat: integrity chain

--- tests/cli.rs
+++ tests/cli.rs
@@ -65,3 +65,21 @@ fn help_and_version() {
 
 #[test]
 fn add_list_and_dedupe() {
+    let dir = fresh_dir("add");
+    let a = run(
+        &dir,
+        &[
+            "add",
+            "--kind",
+            "semantic",
+            "--content",
+            "prod region is eu-west-1",
+            "--tags",
+            "infra",
+            "--now",
+            "1000",
+        ],
+    );
+    assert_eq!(a.code, 0, "stderr: {}", a.stderr);
+    assert!(a.stdout.starts_with("added "));
+
