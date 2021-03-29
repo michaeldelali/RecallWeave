a41c651ef7c05eb38077171a7e95f28985c88371
michaeldelali <43325182+michaeldelali@users.noreply.github.com>
2021-03-29 11:27:00 +0700

    docs: document store layout

---
+++ tests/lifecycle.rs
@@ -0,0 +1,22 @@
+//! End-to-end lifecycle tests exercised through the public library API.
+//!
+//! These tests build a store in a fresh temp directory, drive it through a full
+//! lifecycle (assert -> dedupe -> supersede -> forget -> compact), and assert on
+//! integrity and the exported pack at each stage. They complement the focused
+//! unit tests inside each module.
+
+use recallweave::json::Json;
+use recallweave::model::{MemoryKind, Provenance};
+use recallweave::query::{Query, Sort};
+use recallweave::store::{AssertSpec, Store};
+use std::env;
+use std::path::PathBuf;
+use std::time::{SystemTime, UNIX_EPOCH};
+
+fn fresh_dir(tag: &str) -> PathBuf {
+    let nanos = SystemTime::now()
+        .duration_since(UNIX_EPOCH)
+        .map(|d| d.as_nanos())
+        .unwrap_or(0);
+    let mut dir = env::temp_dir();
+    dir.push(format!(
