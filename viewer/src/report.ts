4293d2e8015df34e54e39d1aca50b9eaf2a7f954
michaeldelali <43325182+michaeldelali@users.noreply.github.com>
2023-02-24 15:03:00 +0700

    test: cover cli runner

---
+++ viewer/src/report.ts
@@ -0,0 +1,8 @@
+/**
+ * Renders a memory-pack as a plain-text report for the terminal.
+ *
+ * This is the default output of the `tapestry` CLI when no `--svg` is requested.
+ * It is intentionally compact and dependency-free (no colour libraries) so it
+ * behaves in any terminal and in CI logs.
+ */
+
