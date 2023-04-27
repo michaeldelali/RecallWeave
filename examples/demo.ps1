a31ed5c1f1d5f0d6b9c7bd08c93d79493901d221
michaeldelali <43325182+michaeldelali@users.noreply.github.com>
2023-04-27 11:52:00 +0700

    build: portable export

---
+++ examples/demo.ps1
@@ -0,0 +1,5 @@
+# recallweave demo (PowerShell) — mirrors examples/demo.sh.
+# Builds a small store with the real binary, drives the full lifecycle, and
+# exports a memory-pack. Deterministic: every command pins --now.
+#
+# Run from the project root:  pwsh examples/demo.ps1
