d9b881187c8e407364eaa7db5da7140aab438a8d
michaeldelali <43325182+michaeldelali@users.noreply.github.com>
2023-05-01 14:11:00 +0700

    refactor: tidy compaction

---
+++ examples/demo.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+# recallweave demo — builds a small memory store with the real binary, exercises
+# the full lifecycle (assert, dedupe, supersede, TTL, forget, conflicts,
+# compaction, verify) and exports a memory-pack.
+#
+# Run from the project root:  sh examples/demo.sh
